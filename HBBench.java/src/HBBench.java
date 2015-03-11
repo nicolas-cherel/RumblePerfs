fce4c3f4e838fcb4168e67d009abe9eb6df03751
Nicolas Cherel <nicolas.cherel@gmail.com>
2015-03-11 19:05:13 +0100

    refactor measurement

--- HBBench.java/src/HBBench.java
+++ HBBench.java/src/HBBench.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
+import java.util.function.Function;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.jknack.handlebars.Handlebars;
@@ -25,8 +26,12 @@ public class HBBench {
 		this.stringTemplate = new String(content, StandardCharsets.UTF_8);
 	}
 	
-	void compile() throws IOException {
-		hb.compileInline(this.stringTemplate);
+	void compile() {
+		try {
+			hb.compileInline(this.stringTemplate);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	void prepareContent() throws IOException {
@@ -40,9 +45,14 @@ public class HBBench {
 		this.data = new ObjectMapper().readValue(dataContent, List.class);
 	}
 	
-	public void expansion() throws IOException {
+	public void expansion() {
 		StringWriter w = new StringWriter();
-		this.t.apply(this.data, w);
+		try {
+			this.t.apply(this.data, w);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	static class Stats {
@@ -80,6 +90,35 @@ public class HBBench {
 		
 	}
 	
+	public static <T> void measure(HashMap<String, Stats> stats, String setName, Function<Void, Void> c)  {
+    	{
+    		long start = System.nanoTime();
+    		c.apply(null);
+    		stats.get(setName).setCold(System.nanoTime() - start);    		
+    	}
+    	
+    	
+    	for (int i = 0; i < 1000; i++) {
+    		// warm up
+    		c.apply(null);
+    	}
+    	
+    	{
+    		int iterations = 50;
+    		long start = System.nanoTime();
+    		long max = Long.MIN_VALUE;
+    		long min = Long.MAX_VALUE;
+    		for (int i = 0; i < iterations; i++) {
+    			long setpStart  = System.nanoTime();
+        		c.apply(null);
+    			max = Math.max(max, System.nanoTime() - setpStart);
+    			min = Math.min(min, System.nanoTime() - setpStart);
+    		}
+    		stats.get(setName).setAvg((System.nanoTime() - start)/iterations);
+    		stats.get(setName).setDelta(max - min);
+    	}
+	}
+	
     public static void main(String[] args) throws IOException {
     	HBBench b = new HBBench();
     	HashMap<String, Stats> stats = new HashMap<String, Stats>();
@@ -87,61 +126,11 @@ public class HBBench {
     	stats.put("expansion", new Stats());
 
     	b.prepareCompile();
-    	
-    	{
-    		long start = System.nanoTime();
-    		b.compile();
-    		stats.get("compile").setCold(System.nanoTime() - start);    		
-    	}
-    	
-    	
-    	for (int i = 0; i < 10; i++) {
-    		// warm up
-    		b.compile();
-    	}
-    	
-    	{    		
-    		long start = System.nanoTime();
-    		long max = Long.MIN_VALUE;
-    		long min = Long.MAX_VALUE;
-    		for (int i = 0; i < 10; i++) {
-    			long setpStart  = System.nanoTime();
-    			b.compile();
-    			max = Math.max(max, System.nanoTime() - setpStart);
-    			min = Math.min(min, System.nanoTime() - setpStart);
-    		}
-    		stats.get("compile").setAvg((System.nanoTime() - start)/10);
-    		stats.get("compile").setDelta(max - min);
-    	}
+    	measure(stats, "compile", (Void) -> { b.compile(); return null; });
     	
     	b.prepareContent();
-    	
+    	measure(stats, "expansion", (Void) -> { b.expansion(); return null; });
 
-    	{
-    		long start = System.nanoTime();
-    		b.expansion();
-    		stats.get("expansion").setCold(System.nanoTime() - start);    		
-    	}
-    	
-    	for (int i = 0; i < 10; i++) {
-    		// warm up
-    		b.expansion();
-    	}
-    	
-    	
-    	{
-    		long start = System.nanoTime();
-    		long max = Long.MIN_VALUE;
-    		long min = Long.MAX_VALUE;
-    		for (int i = 0; i < 10; i++) {
-    			long setpStart  = System.nanoTime();
-    			b.expansion();
-    			max = Math.max(max, System.nanoTime() - setpStart);
-    			min = Math.min(min, System.nanoTime() - setpStart);
-    		}
-    		stats.get("expansion").setAvg((System.nanoTime() - start)/10);
-    		stats.get("expansion").setDelta(max - min);
-    	}
 
     	System.out.println(stats);    	
     }
