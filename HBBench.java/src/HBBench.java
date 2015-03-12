f86abd02a24b9526570b0740320875f476cc2bd2
Nicolas Cherel <nicolas.cherel@gmail.com>
2015-03-12 18:03:26 +0100

    add another template for benchs, attempt to mitigate JVM warming with another dataset

--- HBBench.java/src/HBBench.java
+++ HBBench.java/src/HBBench.java
@@ -3,9 +3,11 @@ import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.function.Function;
+import java.util.Map.Entry;
+import java.util.function.Consumer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.jknack.handlebars.Handlebars;
@@ -15,27 +17,51 @@ import com.github.jknack.handlebars.Template;
 
 public class HBBench {
 	
-	String stringTemplate;
+	String warmupCompile = "{{#this}} will not show {{missingContent}}{{! and comment}} "
+			+ "but can expand this {{text}} {{else}}{{that}}{{/this}}"
+			+ "{{#list}}found {{element}} in list {{/list}}";
+	Template warmupTemplate = null;
+	Object warmUpData = null;
+	
+	HashMap<String, String> stringTemplate;
 	Handlebars hb;
-	Template t;
-	Object data;
 	
 	void prepareCompile() throws IOException {
 		this.hb = new Handlebars();
-		byte[] content = Files.readAllBytes(Paths.get("../data/template.hbs"));
-		this.stringTemplate = new String(content, StandardCharsets.UTF_8);
+		
+		this.stringTemplate = new HashMap<String, String>();
+		for(String f: Arrays.asList("template", "helper")) {
+			byte[] content;
+			content = Files.readAllBytes(Paths.get("../data/" + f +".hbs"));
+			this.stringTemplate.put(f, new String(content, StandardCharsets.UTF_8));
+		}
+
 	}
 	
-	void compile() {
+	void warmupCompile() {
 		try {
-			hb.compileInline(this.stringTemplate);
+			hb.compileInline(this.warmupCompile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	void compile(String s) {
+		try {
+			hb.compileInline(s);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	Template t;
+	Object data;
 
 	void prepareContent() throws IOException {
-
+		this.warmUpData = new ObjectMapper().readTree("{\"text\": \"some string\", \"elemet\": [1, 2, 3, 4]}");
+		this.warmupTemplate = hb.compileInline(this.warmupCompile);
+		
 		byte[] content = Files.readAllBytes(Paths.get("../data/template.hbs"));
 		String tContent = new String(content, StandardCharsets.UTF_8);
 		byte[] dataSource = Files.readAllBytes(Paths.get("../data/template.json"));
@@ -43,6 +69,16 @@ public class HBBench {
 		
 		this.t = hb.compileInline(tContent);
 		this.data = new ObjectMapper().readValue(dataContent, List.class);
+	}
+	
+	public void warmupExpansion() {
+		StringWriter w = new StringWriter();
+		try {
+			this.warmupTemplate.apply(this.warmUpData, w);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void expansion() {
@@ -90,27 +126,27 @@ public class HBBench {
 		
 	}
 	
-	public static <T> void measure(HashMap<String, Stats> stats, String setName, Function<Void, Void> c)  {
+	public static <T> void measure(HashMap<String, Stats> stats, String setName, Consumer<Void> warmup, Consumer<Void> c)  {
     	{
     		long start = System.nanoTime();
-    		c.apply(null);
+    		c.accept(null);
     		stats.get(setName).setCold(System.nanoTime() - start);    		
     	}
     	
     	
     	for (int i = 0; i < 1000; i++) {
     		// warm up
-    		c.apply(null);
+    		warmup.accept(null);
     	}
     	
     	{
-    		int iterations = 50;
+    		int iterations = 20;
     		long start = System.nanoTime();
     		long max = Long.MIN_VALUE;
     		long min = Long.MAX_VALUE;
     		for (int i = 0; i < iterations; i++) {
     			long setpStart  = System.nanoTime();
-        		c.apply(null);
+    			c.accept(null);
     			max = Math.max(max, System.nanoTime() - setpStart);
     			min = Math.min(min, System.nanoTime() - setpStart);
     		}
@@ -121,18 +157,21 @@ public class HBBench {
 	
     public static void main(String[] args) throws IOException {
     	HBBench b = new HBBench();
-    	HashMap<String, Stats> stats = new HashMap<String, Stats>();
-    	stats.put("compile", new Stats());
-    	stats.put("expansion", new Stats());
-
     	b.prepareCompile();
-    	measure(stats, "compile", (Void) -> { b.compile(); return null; });
     	
-    	b.prepareContent();
-    	measure(stats, "expansion", (Void) -> { b.expansion(); return null; });
+    	for (Entry<String, String> t: b.stringTemplate.entrySet()) {
+        	HashMap<String, Stats> stats = new HashMap<String, Stats>();
+        	stats.put("compile", new Stats());
+        	stats.put("expansion", new Stats());
+
+        	measure(stats, "compile", (Void) -> b.warmupCompile(), (Void) -> b.compile(t.getValue()));
+        	
+        	b.prepareContent();
+        	measure(stats, "expansion", (Void) -> b.warmupExpansion(), (Void) -> b.expansion());
 
 
-    	System.out.println(stats);    	
+        	System.out.println(t.getKey() + " " + stats);
+    	}    	
     }
 
 
--- HBBench.rust/src/lib.rs
+++ HBBench.rust/src/lib.rs
@@ -48,3 +48,32 @@ fn expansion(b: &mut Bencher) {
       ::rumblebars::eval(&t, &j, &mut out, &::std::default::Default::default())  
     })
 }
+
+#[bench]
+fn helper_expansion(b: &mut Bencher) {
+    let t = {
+      let mut f = File::open("../data/helper.hbs").unwrap();
+      let mut s = String::new();
+      f.read_to_string(&mut s);
+      s.parse().unwrap_or_else(|e| {
+        println!("cannot parse template");
+        panic!(format!("{:?}", e))
+      })
+    };
+
+    let j = {
+      let mut f = File::open("../data/template.json").unwrap();
+      let mut s = String::new();
+      f.read_to_string(&mut s);
+      s.parse::<::serialize::json::Json>().unwrap_or_else(|e| {
+        println!("cannot parse json data");
+        panic!(format!("{:?}", e))
+      })
+    };
+
+    
+    b.iter(|| {
+      let mut out = Vec::<u8>::new();
+      ::rumblebars::eval(&t, &j, &mut out, &::std::default::Default::default())  
+    })
+}
